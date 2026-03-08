345ac2eba6a3463adb787dc62c7ac4fcfbfd4177
hza81007155 <800755@qq.com>
2026-03-09 07:39:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,14 +37,16 @@ sed -i 's/192.168.1.1/192.168.10.12/g' package/base-files/luci/bin/config_genera
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
 #主题
-rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/kenzo/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/kenzo/luci-app-argon-config
+#git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/kenzo/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/kenzo/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+git clone https://github.com/hza81007155/luci-theme-argone
+git clone https://github.com/hza81007155/luci-app-argone-config
 
 # 微信推送
 rm -rf feeds/kenzo/luci-app-wechatpush
